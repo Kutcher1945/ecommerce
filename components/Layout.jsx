3e6593b01e71b55189d50114678af8277441e776
Adilan <adilan.akhramovich@gmail.com>
2024-02-03 18:19:34 +0800

    aded a new proj

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>JS Mastery Store</title>
+        <title>Магазин Колготок</title>
       </Head>
       <header>
         <Navbar />
