9cbb670ff719da616b537d49d821b276f01261f2
Adilan <adilan.akhramovich@gmail.com>
2024-02-03 21:32:11 +0800

    aded a new proj

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -10,15 +10,6 @@ const HeroBanner = ({ heroBanner }) => {
         <p className="beats-solo">{heroBanner.smallText}</p>
         <h3>{heroBanner.midText}</h3>
         <h1>{heroBanner.largeText1}</h1>
-        <div>
-          <Link href={`/product/${heroBanner.product}`}>
-            <button type="button">{heroBanner.buttonText}</button>
-          </Link>
-          <div className="desc">
-            <h5>Description</h5>
-            <p>{heroBanner.desc}</p>
-          </div>
-        </div>
       </div>
     </div>
   )
