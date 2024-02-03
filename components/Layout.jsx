db52ce84898dd86086dc1d14a9b089dd79e7371d
Adilan <adilan.akhramovich@gmail.com>
2024-02-04 01:30:41 +0600

    added a whatsapp and changed styles

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -4,11 +4,28 @@ import Head from 'next/head';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
+export const metadata = {
+  title: 'Магазин Колготок',
+  description: 'Добро пожаловать на наш вебсайт, специализирующийся на продаже высококачественных колготок...',
+  keywords: [
+    'женские колготки',
+    'колготки черного цвета',
+    'плотные колготки',
+    'колготки с рисунком',
+    'колготки',
+    'колготки Алматы',
+  ],
+}
+
+
 const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>Магазин Колготок</title>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        {metadata.keywords && <meta name="keywords" content={metadata.keywords.join(', ')} />}
+        {/* Add more common SEO meta tags here */}
       </Head>
       <header>
         <Navbar />
@@ -20,7 +37,7 @@ const Layout = ({ children }) => {
         <Footer />
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
