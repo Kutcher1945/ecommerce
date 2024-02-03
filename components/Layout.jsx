390ba340eabf27f23f131c11f512ccc1cb86d401
Adilan <adilan.akhramovich@gmail.com>
2024-02-04 01:48:59 +0600

    added a new metrica

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -15,8 +15,7 @@ export const metadata = {
     'колготки',
     'колготки Алматы',
   ],
-}
-
+};
 
 const Layout = ({ children }) => {
   return (
@@ -26,6 +25,24 @@ const Layout = ({ children }) => {
         <meta name="description" content={metadata.description} />
         {metadata.keywords && <meta name="keywords" content={metadata.keywords.join(', ')} />}
         {/* Add more common SEO meta tags here */}
+        
+        {/* Yandex.Metrika counter */}
+        <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `
+          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+          ym(96354693, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true,
+            webvisor:true
+          });
+        `}}></script>
+        <noscript><div><img src="https://mc.yandex.ru/watch/96354693" style={{ position: 'absolute', left: '-9999px' }} alt="" /></div></noscript>
+        {/* /Yandex.Metrika counter */}
       </Head>
       <header>
         <Navbar />
