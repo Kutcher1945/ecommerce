36a0a5d5c197768d0a8206e2ff2361142f41dd6d
Adilan <adilan.akhramovich@gmail.com>
2024-02-03 21:43:28 +0800

    aded a new proj

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <div className="navbar-container">
       <p className="logo">
-        <Link href="/">Lady Dimitresku</Link>
+        <Link href="/">На главную</Link>
       </p>
 
       <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
