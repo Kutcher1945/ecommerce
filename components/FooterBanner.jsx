95f892de64ce0c509bcc540ec95ac501a79d3707
Adilan <adilan.akhramovich@gmail.com>
2024-02-03 21:29:07 +0800

    aded a new proj

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 import { urlFor } from '../lib/client';
 
-const FooterBanner = ({ footerBanner: { discount, largeText1, largeText2, saleTime, smallText, midText, desc, product, buttonText, image } }) => {
+const FooterBanner = ({ footerBanner: { discount, largeText1, largeText2, saleTime, smallText, midText, desc, product, buttonText} }) => {
   return (
     <div className="footer-banner-container">
       <div className="banner-desc">
@@ -17,14 +17,10 @@ const FooterBanner = ({ footerBanner: { discount, largeText1, largeText2, saleTi
           <p>{smallText}</p>
           <h3>{midText}</h3>
           <p>{desc}</p>
-          <Link href={`/product/₸{product}`}>
+          <Link href={`/product/${product}`}>
             <button type="button">{buttonText}</button>
           </Link>
         </div>
-
-        <img 
-          src={urlFor(image)} className="footer-banner-image"
-        />
       </div>
     </div>
   )
