fe0464876c1c3e6044a1dc23ef8b6860bd53470e
Adilan <adilan.akhramovich@gmail.com>
2024-02-03 22:57:32 +0600

    added a new p

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,8 @@ const Home = ({ products, bannerData }) => (
   <div>
     <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
     <div className="products-heading">
-      <h2>Best Seller Products</h2>
-      <p>speaker There are many variations passages</p>
+      <h2>Список лучших товаров</h2>
+      <p>Откройте для себя широкий ассортимент высококачественных товаров с выдающимися характеристиками. Наши лучшие товары известны своей надежностью и ценностью.</p>
     </div>
 
     <div className="products-container">
