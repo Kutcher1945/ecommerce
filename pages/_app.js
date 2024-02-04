0aed01c1a4a7601860c14efbd4449ccc086eaa54
Adilan <adilan.akhramovich@gmail.com>
2024-02-04 07:04:07 +0600

    added a new metrica

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Toaster } from 'react-hot-toast';
-
+import { DefaultSeo } from 'next-seo';
 import { Layout } from '../components';
 import '../styles/globals.css';
 import { StateContext } from '../context/StateContext';
@@ -8,12 +8,45 @@ import { StateContext } from '../context/StateContext';
 function MyApp({ Component, pageProps }) {
   return (
     <StateContext>
+      <DefaultSeo
+        title="Купить колготки в Магазине Колготок" // Set your default title
+        description="Магазин Колготок – ваш источник стильных и высококачественных колготок. Широкий ассортимент и быстрая доставка." // Set your default description
+        openGraph={{
+          title: "Купить колготки в Магазине Колготок", // Set your default Open Graph title
+          description: "Магазин Колготок – ваш источник стильных и высококачественных колготок. Широкий ассортимент и быстрая доставка.", // Set your default Open Graph description
+          type: 'website',
+          locale: 'ru_RU', // Set your default locale to Russian
+          url: 'https://www.example.com/', // Replace with your website URL
+          site_name: 'Магазин Колготок', // Set your default site name
+          images: [
+            {
+              url: 'https://www.example.com/og-image.jpg', // Replace with your Open Graph image URL
+              width: 1200,
+              height: 630,
+              alt: 'Магазин Колготок', // Replace with an appropriate alt text
+            },
+          ],
+          // Add other Open Graph properties here
+        }}
+        twitter={{
+          handle: '@magazinkolgotok', // Replace with your Twitter handle
+          site: '@magazinkolgotok', // Replace with your Twitter site
+          cardType: 'summary_large_image',
+        }}
+        // Add your keywords relevant to the tights store here
+        additionalMetaTags={[
+          {
+            name: 'keywords',
+            content: 'колготки, чулки, белье, мода', // Add keywords relevant to your store
+          },
+        ]}
+      />
       <Layout>
         <Toaster />
         <Component {...pageProps} />
       </Layout>
     </StateContext>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
